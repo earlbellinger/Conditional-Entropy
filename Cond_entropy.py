07e6df230a8946259c64927a2fdd9f348ba3ff3d
Earl Bellinger <earlbellinger@gmail.com>
2014-07-10 14:05:42 -0400

    Formatting & some optimizations

--- Cond_entropy.py
+++ Cond_entropy.py
@@ -3,13 +3,11 @@ from periodogram import find_period, rephase, get_phase
 import matplotlib.pyplot as plt
 
 def hc(bins, row, col, size):
-
-        hc1 = [(bins[i][j]/len(size))*numpy.log(sum(bins[m_i][j]/len(size) for m_i in row)/(bins[i][j]/len(size))) for i in row for j in col]
-        s = numpy.nansum(hc1)
-
-        return s
-
-
+    hc1 = ((bins[i][j] / size) \
+           * numpy.log(sum(bins[m_i][j] / size for m_i in row) \
+                       / (bins[i][j] / size))
+           for i in row for j in col)
+    return numpy.nansum(numpy.fromiter(hc1, dtype="float"))
 
 data = numpy.ma.array(data=numpy.loadtxt('OGLE-LMC-CEP-2147.dat'), mask=None, dtype=float)
 
@@ -22,13 +20,13 @@ col = numpy.linspace(0,4,5)
 entropy = []
 
 for p in periods:
-	r = rephase(data, p, 0)
+    r = rephase(data, p, 0)
 
-	normalized = numpy.ma.copy(r)
-	normalized.T[1] = [ (x[1] - numpy.min(r.T[1]))/(numpy.max(r.T[1])-numpy.min(r.T[1])) for x in normalized]
+    normalized = numpy.ma.copy(r)
+    normalized.T[1] = [ (x[1] - numpy.min(r.T[1]))/(numpy.max(r.T[1])-numpy.min(r.T[1])) for x in normalized]
 
-	bins, binsX, binsY = numpy.histogram2d(normalized.T[0],normalized.T[1], [10,5], [[0,1],[0,1]])
-	entropy.append( hc(bins, row, col, normalized.T[1]))
+    bins, binsX, binsY = numpy.histogram2d(normalized.T[0], normalized.T[1], [10,5], [[0,1],[0,1]])
+    entropy.append(hc(bins, row, col, len(normalized.T[1])))
 
 print (entropy)
 print (periods)
